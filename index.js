ad436dbe5c6207dbb541ba83731e7e79c622852d
Feruser S. Spectrum <supakit.sjd@gmail.com>
2019-04-30 17:23:04 +0700

    make it better

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 const app = express();
+  
   app.use(express.static(path.join(__dirname, 'public')));
   app.set('views', path.join(__dirname, 'views'));
   app.use("/CSS",express.static("CSS"));
@@ -9,6 +10,10 @@ const app = express();
 
   app.get("/",function(req,res){
     res.render("Login");
-  })
+  });
+
+  app.get("/Signup",function(req,res){
+    res.render("Signup");
+  });
   
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
