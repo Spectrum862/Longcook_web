7368c807aaad80b907abf6ac4bbe00146bb923c7
Feruser S. Spectrum <supakit.sjd@gmail.com>
2019-04-30 16:17:46 +0700

    make it better

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const app = express()
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
   app.use(express.static(path.join(__dirname, 'public')));
   app.set('views', path.join(__dirname, 'views'));
   app.use("/CSS",express.static("CSS"));
   app.set('view engine', 'ejs');
 
   app.get("/",function(req,res){
-    res.send("Hello Ja");
+    res.render("Login");
   })
   
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
