ad3473e3d9d109e1d892f61cbb46fc450332c5e3
Feruser S. Spectrum <supakit.sjd@gmail.com>
2019-04-30 17:50:41 +0700

    make it better

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
   app.use(express.static(path.join(__dirname, 'public')));
   app.set('views', path.join(__dirname, 'views'));
   app.use("/CSS",express.static("CSS"));
-  app.use("/views",express.static("views"));
   app.set('view engine', 'ejs');
 
   app.get("/login",function(req,res){
